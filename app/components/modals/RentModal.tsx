8191aa4542941f2c047080d1c069549c09bc747e
RiP3rQ <rafalpompa2000@gmail.com>
2023-04-13 00:16:35 +0200

    step 5/6 (description / price) + server POST route using prisma

--- app/components/modals/RentModal.tsx
+++ app/components/modals/RentModal.tsx
@@ -6,11 +6,15 @@ import useRentModal from "@/app/hooks/useRentModal";
 import Heading from "../Heading";
 import { categories } from "../Navbar/Categories";
 import CategoryInput from "../inputs/CategoryInput";
-import { FieldValues, useForm } from "react-hook-form";
+import { FieldValues, SubmitHandler, useForm } from "react-hook-form";
 import CountrySelect from "../inputs/CountrySelect";
 import dynamic from "next/dynamic";
 import Counter from "../inputs/Counter";
 import ImageUpload from "../inputs/ImageUpload";
+import Input from "../inputs/Input";
+import axios from "axios";
+import { toast } from "react-hot-toast";
+import { useRouter } from "next/navigation";
 
 type Props = {};
 
@@ -25,6 +29,7 @@ enum STEPS {
 
 const RentModal = (props: Props) => {
   const rentModal = useRentModal();
+  const router = useRouter();
 
   // --------------- form state
   const {
@@ -58,6 +63,7 @@ const RentModal = (props: Props) => {
   // step 4
   const imageSrc = watch("imageSrc");
 
+  // dynamic import for map component
   const Map = useMemo(
     () =>
       dynamic(() => import("../Map"), {
@@ -66,6 +72,7 @@ const RentModal = (props: Props) => {
     [location]
   );
 
+  // set custom value for react-hook-form
   const setCustomValue = (id: string, value: any) => {
     setValue(id, value, {
       shouldValidate: true,
@@ -74,8 +81,34 @@ const RentModal = (props: Props) => {
     });
   };
 
+  // --------------- submit logic
+  const onSubmit: SubmitHandler<FieldValues> = (data) => {
+    if (step !== STEPS.PRICE) {
+      return onNext();
+    }
+
+    setIsLoading(true);
+
+    axios
+      .post("/api/listings", data)
+      .then((res) => {
+        toast.success("Listing Created!");
+        router.refresh();
+        reset();
+        setStep(STEPS.CATEGORY);
+        rentModal.onClose();
+      })
+      .catch((err) => {
+        toast.error("Something went wrong!");
+      })
+      .finally(() => {
+        setIsLoading(false);
+      });
+  };
+
   // --------------- steps logic
   const [step, setStep] = useState(STEPS.CATEGORY);
+  const [isLoading, setIsLoading] = useState(false);
 
   const onBack = () => {
     setStep((value) => value - 1);
@@ -210,6 +243,23 @@ const RentModal = (props: Props) => {
           title="How would you describe your place?"
           subtitle="Short and sweet works best!"
         />
+        <Input
+          id="title"
+          label="Title"
+          disabled={isLoading}
+          register={register}
+          errors={errors}
+          required
+        />
+        <hr />
+        <Input
+          id="description"
+          label="Description"
+          disabled={isLoading}
+          register={register}
+          errors={errors}
+          required
+        />
       </div>
     );
   }
@@ -223,6 +273,16 @@ const RentModal = (props: Props) => {
           title="Now, set your price"
           subtitle="How much do you charge per night?"
         />
+        <Input
+          id="price"
+          label="Price"
+          formatPrice
+          type="number"
+          disabled={isLoading}
+          register={register}
+          errors={errors}
+          required
+        />
       </div>
     );
   }
@@ -232,7 +292,7 @@ const RentModal = (props: Props) => {
       isOpen={rentModal.isOpen}
       title="Airbnb your home"
       onClose={rentModal.onClose}
-      onSubmit={onNext}
+      onSubmit={handleSubmit(onSubmit)}
       actionLabel={actionLabel}
       secondaryActionLabel={secondaryActionLabel}
       secondaryAction={step === STEPS.CATEGORY ? undefined : onBack}
